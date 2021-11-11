be04b85bc198a209fbe22d3b0430b0dcd22f580a
Ben Hsieh <bqh5026@gmail.com>
2021-11-11 11:14:08 -0600

    Add phone sign in form group

--- src/app/me/me.page.ts
+++ src/app/me/me.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-me',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MePage implements OnInit {
 
+  form: FormGroup;
+  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+  type: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      phone: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(10), Validators.maxLength(10)]
+      }),
+        password: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(8)]
+      }),
+    });
+  }
+
+  changeType() {
+    this.type = !this.type;
   }
 
 }
